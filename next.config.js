a409fef0ec6157ed1518fa44454afe9902500683
Michele Lunati <lunatimichele@gmail.com>
2023-11-22 17:59:41 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    dirs: ["."],
+  },
+  poweredByHeader: false,
+  // trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  basePath: "",
+  distDir: "out",
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
